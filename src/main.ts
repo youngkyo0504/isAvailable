c1c6eaea9bd6c1fd58dd87d2fb8f8f0f61424f43
keumk <keumky2@gmail.com>
2022-03-01 01:07:06 +0900

    sfsdf

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function main() {
     const result = await crawling(URL);
     salesPossible.push(result);
   }
-  setOutput('result', 'keum');
+  setOutput('result', salesPossible.join('\n'));
   console.log(salesPossible.join('\n'));
 }
 
