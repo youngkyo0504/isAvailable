4394176c58c16644dfb406d21d2a755e7c46dc93
keumk <keumky2@gmail.com>
2022-03-01 00:33:42 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { JSDOM } from 'jsdom';
+import axios from 'axios';
+
+const URL_LIST = [
+  'https://www.jooyonshop.co.kr/goods/goods_view.php?goodsNo=1000000233',
+  'https://www.jooyonshop.co.kr/goods/goods_view.php?goodsNo=1000000230',
+  'https://www.jooyonshop.co.kr/goods/goods_view.php?goodsNo=1000000166',
+  'https://www.jooyonshop.co.kr/goods/goods_view.php?goodsNo=1000000165',
+];
+
+async function crawling(URL: string) {
+  const response = await axios.get(URL);
+  const data = response.data;
+  const dom = new JSDOM(data);
+  const soldOutBtn = dom.window.document.querySelector('.btn_add_soldout');
+  const addOrderBtn = dom.window.document.querySelector('.btn_add_order');
+  return !soldOutBtn && addOrderBtn ? `구매 가능합니다. ${URL}` : '구매 불가합니다.';
+}
+
+async function main() {
+  const salesPossible = [];
+  for (const URL of URL_LIST) {
+    const result = await crawling(URL);
+    salesPossible.push(result);
+  }
+  console.log(salesPossible.join('\n'));
+}
+
+main();
